229c14da765e9cff8fef8ee109613d32e2bb068c
Ryuho Kudo <ryuhokudo@gmail.com>
2010-09-25 17:25:07 -0700

    finished cvs parser

--- lab1/src/MyParser.java
+++ lab1/src/MyParser.java
@@ -1,20 +1,59 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 
 public class MyParser {
-	ArrayList<Vector> parseNumCSV(String filename)
+	static ArrayList<Vector> parseNumCSV(String filename)
 	{
+		ArrayList<Vector> answer = new ArrayList<Vector>();
+		
+		FileInputStream fstream = null;
 		try {
-			FileInputStream fstream = new FileInputStream("textfile.txt");
+			fstream = new FileInputStream(filename);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
 			System.err.println("File Not Found.");
 		}
 		
+		DataInputStream in = new DataInputStream(fstream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String strLine;
 		
-		return null;
+		try {
+			while ((strLine = br.readLine()) != null)   {
+				if(strLine.indexOf(",") != -1){
+					answer.add(stringToVec(strLine));
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("Exception while reading file.");
+		}
+		
+
+		return answer;
+	}
+
+	static Vector stringToVec(String s){
+		s = s.replaceAll("^\\s*,", "0,");
+		s = s.replaceAll(",{2}", ",0,");
+		s = s.replaceAll(",{2}", ",0,");
+		s = s.replaceAll(",\\s*$", ",0");
+
+		Vector answer = new Vector();
+
+		StringTokenizer st = new StringTokenizer(s,",");
+
+		
+		while (st.hasMoreTokens()) {
+			try{
+				answer.addElement(Double.parseDouble(st.nextToken()));
+			}catch(java.lang.NumberFormatException e){
+				System.err.println("");
+			}
+		}
+		
+		return answer;
 	}
 	
 	//ArrayList<TextToken> parseTextFile(String filename)
