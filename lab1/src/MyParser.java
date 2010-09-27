759a1fae57b5ac825d842d2317d0611221e58b82
Ryuho Kudo <ryuhokudo@gmail.com>
2010-09-27 15:06:03 -0700

    Fixed a bug

--- lab1/src/MyParser.java
+++ lab1/src/MyParser.java
@@ -22,8 +22,13 @@ public class MyParser {
 		try {
 			while ((strLine = br.readLine()) != null)
 				{
-				answer.add(stringToVec(strLine));
-			}
+					Vector tempVec = stringToVec(strLine);
+					if(tempVec != null)
+					{
+						answer.add(tempVec);
+					}
+					
+				}
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.err.println("Exception while reading file.");
@@ -39,18 +44,24 @@ public class MyParser {
 		s = s.replaceAll(",{2}", ",0,");
 		s = s.replaceAll(",\\s*$", ",0");
 
+		boolean empyString = true;
 		Vector answer = new Vector();
 
 		StringTokenizer st = new StringTokenizer(s,",");
 
 		while (st.hasMoreTokens()) {
+			empyString = false;
 			try{
 				answer.addElement(Double.parseDouble(st.nextToken()));
+				empyString = false;
 			}catch(java.lang.NumberFormatException e){
-				System.err.println("");
+				answer.addElement(0.0);
 			}
 		}
 		
+		if(empyString){
+			return null;
+		}
 		return answer;
 	}
 	
@@ -86,8 +97,6 @@ public class MyParser {
 	}
 
 	static ArrayList<TextToken> stringToTextTokens(String s, ArrayList<TextToken> tokens){
-		System.out.print("s= "+ s + "|");
-		System.out.println("s.length() = "+ s.length());
 		if(s.isEmpty() && !(tokens.get(tokens.size()-1) instanceof EOP)){
 			if(!(tokens.get(tokens.size()-1) instanceof EOS))
 			{
@@ -108,12 +117,10 @@ public class MyParser {
 				boolean done = false;
 				while(!done)
 				{
-					System.out.println("loop str: " + currChunk);
 					
 					if(currChunk.length() >= 4 && 
 							currChunk.substring(currChunk.length()-4, currChunk.length()).compareTo("....") == 0)
 					{
-						System.out.println("Elipse with period found!");
 						bufferTokens.add(new Chars("..."));
 						bufferTokens.add(new Chars("."));
 						currChunk = currChunk.substring(0,currChunk.length()-4);
@@ -121,93 +128,78 @@ public class MyParser {
 					else if(currChunk.length() >= 3 && 
 							currChunk.substring(currChunk.length()-3, currChunk.length()).compareTo("...") == 0)
 					{
-						System.out.println("Elipse found!");
 						bufferTokens.add(new Chars("..."));
 						currChunk = currChunk.substring(0,currChunk.length()-3);
 					}
 					else if(currChunk.charAt(0) == '-' && currChunk.length() ==1)
 					{
-						System.out.println("dash found!");
 						tokens.add(new Chars("-"));
 						break;
 					}
 					else if(currChunk.charAt(0) == ':')
 					{
-						System.out.println("Colon found at front!");
 						tokens.add(new Chars(":"));
 						currChunk = currChunk.substring(1,currChunk.length());
 					}
 					else if(currChunk.charAt(0) == '(')
 					{
-						System.out.println("open parenthesis found!");
 						tokens.add(new Chars("("));
 						currChunk = currChunk.substring(1,currChunk.length());
 					}
 					else if(currChunk.charAt(0) == '’')
 					{
-						System.out.println("single quotation mark found at front!");
 						tokens.add(new Chars("\'"));
 						currChunk = currChunk.substring(1,currChunk.length());
 					}
 					else if(currChunk.charAt(0) == '"')
 					{
-						System.out.println("double quotation mark found at front!");
 						tokens.add(new Chars("\""));
 						currChunk = currChunk.substring(1,currChunk.length());
 					}
 					else if(currChunk.charAt(currChunk.length()-1) == '"')
 					{
-						System.out.println("double quotation mark found at end!");
 						bufferTokens.add(new Chars("\""));
 						currChunk = currChunk.substring(0,currChunk.length()-1);
 					}
 					else if(currChunk.charAt(currChunk.length()-1) == '’')
 					{
-						System.out.println("single quotation mark found at end!");
 						bufferTokens.add(new Chars("’"));
 						currChunk = currChunk.substring(0,currChunk.length()-1);
 					}
 					else if(currChunk.charAt(currChunk.length()-1) == ')')
 					{
-						System.out.println("Closing parenthesis found!");
 						bufferTokens.add(new Chars(")"));
 						currChunk = currChunk.substring(0,currChunk.length()-1);
 					}
 					else if(currChunk.charAt(currChunk.length()-1) == ':')
 					{
-						System.out.println("Colon found!");
 						bufferTokens.add(new Chars(":"));
 						currChunk = currChunk.substring(0,currChunk.length()-1);
 					}
 					else if(currChunk.charAt(currChunk.length()-1) == ';')
 					{
-						System.out.println("Semi-colon found!");
 						bufferTokens.add(new Chars(";"));
 						currChunk = currChunk.substring(0,currChunk.length()-1);
 					}
 					else if(currChunk.charAt(currChunk.length()-1) == ',')
 					{
-						System.out.println("Comma found!");
 						bufferTokens.add(new Chars(","));
 						currChunk = currChunk.substring(0,currChunk.length()-1);
 					}
 					else if(currChunk.charAt(currChunk.length()-1) == '.')
 					{
-						System.out.println("Period found!");
 						bufferTokens.add(new Chars("."));
 						bufferTokens.add(new EOS());
 						currChunk = currChunk.substring(0,currChunk.length()-1);
 					}
 					else if(currChunk.charAt(currChunk.length()-1) == '?')
 					{
-						System.out.println("Question mark found!");
 						bufferTokens.add(new Chars("?"));
 						bufferTokens.add(new EOS());
 						currChunk = currChunk.substring(0,currChunk.length()-1);
 					}
 					else if(currChunk.charAt(currChunk.length()-1) == '!')
 					{
-						System.out.println("Exclamation point found!");
 						bufferTokens.add(new Chars("!"));
 						bufferTokens.add(new EOS());
 						currChunk = currChunk.substring(0,currChunk.length()-1);
