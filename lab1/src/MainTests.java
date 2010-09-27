730ab3c24e2e069408d1fb56d2d0301fb4c12c85
Ryuho Kudo <ryuhokudo@gmail.com>
2010-09-27 15:59:30 -0700

    fixed some bugs

--- lab1/src/MainTests.java
+++ lab1/src/MainTests.java
@@ -28,25 +28,25 @@ public class MainTests
 	{
 		ArrayList<Vector> vecList = MyParser.parseNumCSV("data/numFile.txt");
 		System.out.println("Euclidean distance for 1st vector <1, 2, 3, 5>: " + 
-				VectorMath.getEucledianDistance(vecList.get(1)));
+				VectorMath.getEucledianDistance(vecList.get(0)));
 		System.out.println("Euclidean distance between 1st and 2nd vectors: " + 
-				VectorMath.getEucledianDistance(vecList.get(1), vecList.get(2)));
+				VectorMath.getEucledianDistance(vecList.get(0), vecList.get(1)));
 		
 		System.out.println("Dimensional length of 1st vector <1, 2, 3, 5>: " + 
-				VectorMath.getDimensionLength(vecList.get(1)));
+				VectorMath.getDimensionLength(vecList.get(0)));
 		System.out.println("Dot product of first two vectors: " + 
-				VectorMath.getDotProduct(vecList.get(1), vecList.get(2)));
+				VectorMath.getDotProduct(vecList.get(0), vecList.get(1)));
 		System.out.println("Manhattan Distance of 2 vectors: " + 
-				VectorMath.getManhattanDistance(vecList.get(1), vecList.get(2)));
+				VectorMath.getManhattanDistance(vecList.get(0), vecList.get(1)));
 		System.out.println("Pearson Correlation of first two vectors: " + 
-				VectorMath.getPearsonCorrelation(vecList.get(1), vecList.get(2)));
+				VectorMath.getPearsonCorrelation(vecList.get(0), vecList.get(1)));
 		System.out.println(" ");
 		
 		VectorMath.LSMrow(vecList.get(1));
 		VectorMath.LSMcolumn(vecList, 1);
 		
 		System.out.println("Standard Deviation for 1st vector <1, 2, 3, 5>: " + 
-				VectorMath.standardDeviation(vecList.get(1)));
+				VectorMath.standardDeviation(vecList.get(0)));
 		System.out.println("Standard Deviation for 2nd column: " + 
 				VectorMath.standardDeviation(vecList, 1));
 		
@@ -55,7 +55,7 @@ public class MainTests
 	
 	static void testText()
 	{
-		ArrayList<TextToken> lis = MyParser.parseTextFile("data/strFile.txt");
+		ArrayList<TextToken> lis = MyParser.parseTextFile("data/HuckFinn.txt");
 		WordLists wl = new WordLists(lis);
 		
 		System.out.println("Found " + wl.uniqueWords() + " unique words");
@@ -76,7 +76,7 @@ public class MainTests
 		System.out.println("Finding words solid and body in sentence (they are): "
 				+ wl.findTwoWords("solid", "body"));
 		System.out.println("Finding words wait and cube in sentence(they aren't): "
-				+ wl.findTwoWords("solid", "body"));
+				+ wl.findTwoWords("wait", "cube"));
 		System.out.println("Finding words solid and cucumber in sentence(they aren't): "
 				+ wl.findTwoWords("solid", "cucumber"));
 		
