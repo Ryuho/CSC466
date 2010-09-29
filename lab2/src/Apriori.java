61138abdc57531e75fea9aab1d508eb35a43a1e4
KATHYxx <KATHYxxLP@gmail.com>
2010-09-28 17:22:50 -0700

    adding new files to Apriori

---
+++ lab2/src/Apriori.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+
+public class Apriori
+{
+
+	Apriori()
+	{
+		//first pass F1:= {{i}|i e I; support({i})>= minSup);
+	}
+	
+	public void AprMainLoop()
+	{
+		
+	}
+	
+	public ArrayList<Vector> canidateGen()
+	{
+		
+	}
+	
+	public ArrayList<Vector> genRules()
+	{
+		
+	}
+	
+	public ArrayList<Vector> apGenRules()
+	{
+		
+	}
+}
