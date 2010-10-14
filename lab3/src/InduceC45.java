3e8b595e05caa37e2f02c833dbf2d61def6dc658
Ryuho Kudo <ryuhokudo@gmail.com>
2010-10-13 20:09:07 -0700

    added half of the psudo code

--- lab3/src/InduceC45.java
+++ lab3/src/InduceC45.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.traversal.NodeFilter;
+
+import com.sun.org.apache.xerces.internal.dom.DocumentImpl;
+import com.sun.org.apache.xerces.internal.dom.TreeWalkerImpl;
+
 
 
 public class InduceC45{
@@ -7,10 +15,42 @@ public class InduceC45{
         
         Document test = fileParser.parseXMLDomain("data/domain.xml");
 
-        
         csvInfo csvAL = fileParser.parseCSV("data/tree02-20-numbers.csv");
         
-        System.out.println(csvAL);        
+        System.out.println(csvAL);   
+        
+        DocumentImpl tree = new DocumentImpl();
+        Node root = tree.getLastChild(); //why last?
+        AllElements allelements = new AllElements();
+        TreeWalkerImpl walk = (TreeWalkerImpl) tree.createTreeWalker(root, NodeFilter.SHOW_ELEMENT,
+                (NodeFilter) allelements, true);
+        
     }
 
+    
+    public TreeWalkerImpl C45(TreeWalkerImpl tree, ArrayList<ArrayList<Integer>> dataSet, 
+            ArrayList<Integer> attributes, double threshold){
+        //for all dataset
+            // if they are all in the same class
+        
+        //create a leaf node
+        //label the leaf as that class
+        //add it to the tree
+        
+        //find the splitting attribute
+        //if the splitting attribute is null
+            //create a leaf node
+            //label the leaf node with the most frequent category
+            //add the node to tree
+        
+        //else splitting attribute is valid
+            //create a tree node
+            //label the node splitting attribute
+            
+            //for for each different groups the splitting attributes create
+                //
+        
+        return tree;
+    }
 }
+
