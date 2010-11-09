480ce4e6fcfe09901d104691cea758e559966497
Ryuho <ryuhokudo@gmail.com>
2010-11-08 16:06:59 -0800

    done with meanutility

--- lab5/src/src/Query.java
+++ lab5/src/src/Query.java
@@ -1,20 +1,49 @@
 package src;
 
 public class Query {
-	static Csv data;
+	static Csv csv;
 	Query(Csv input){
-		data = input;
+		csv = input;
 	}
 	
-	double MeanUtility(int userId, int itemId){
+	float MeanUtility(int userId, int itemId){
+		//blank out the user's item specified by param
+		csv.data.get(userId).ratings().set(itemId, (float)99);
+		
+		//for each item with valid rating for all users, sum the ratings up
+		float sum = 0;
+		int numOfData = 0;
+		for(int userIdx = 0; userIdx < csv.data.size(); userIdx++){
+			if(csv.data.get(userIdx).ratings().get(itemId) != 99){
+				sum += csv.data.get(userIdx).ratings().get(itemId);
+				numOfData++;
+			}
+		}
+		
+		//now divide it to get the average
+		sum = sum / (float)numOfData;
+		
+		return sum;
+	}
+
+	//sim(u[c],u[c'])
+	// which means a specified c user, find similarity for all ratings (s)
+	float Similarity(int UID1, int UID2){
+		return 0;
+	}
+	
+	float K(int UID1, int UID2){
+		return 0;
+	}
+	
+	float AdjustedWeightedSum(int userId, int itemId){
+		//user's average rating + k * sim * (u(c',s) - uc') 
 		return 99;
 	}
 	
-	double PearsonCorrelation(int userId, int itemId){
+	float PearsonCorrelation(int userId, int itemId){
 		return 99;
 	}
 	
-	double AdjustedWeightedSum(int userId, int itemId){
-		return 99;
-	}
+
 }
