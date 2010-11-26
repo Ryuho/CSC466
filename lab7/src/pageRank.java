244fca456e5bab0741a236f0d375480b121cca12
Ryuho <ryuhokudo@gmail.com>
2010-11-25 18:42:33 -0800

    made a simple parser that can be used for all files

--- lab7/src/pageRank.java
+++ lab7/src/pageRank.java
@@ -1,3 +1,11 @@
-public class pageRank {
 
+
+public class pageRank {
+	// pageRank <fileName>
+	static CSV file;
+	public static void main(String args[]){
+		file = new CSV("data/NCAA_football.csv");
+		
+		System.out.println(file.toString());
+	}
 }
