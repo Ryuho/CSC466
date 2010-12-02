81a63385d6f09af4293d830fe82d600cf2f85e8b
KATHYxx <KATHYxxLP@gmail.com>
2010-12-01 23:16:11 -0800

    split Row into Row and Matrix

---
+++ lab7/src/Matrix.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+
+/*
+ * Call methods in this order:
+ * initMatrix()
+ * addRow() until matrix full
+ * checkIfSquare()  after matrix is full
+ * calcOutInMatrix() to get outgoing links
+ * 
+ * and THEN, you may perform  PageRank
+ */
+public class Matrix
+{
+	public static ArrayList<Row> matrix;
+	
+	static void initMatrix()
+	{
+		matrix = new ArrayList<Row>();
+	}
+	
+	static void addRow(Row val)
+	{
+		if(val.id != -1 )
+		{
+			System.err.println("value already in matrix");
+			System.exit(-1);
+		}
+		matrix.add(val);
+		val.id = matrix.size();
+	}
+	
+	
+	
+	static boolean checkIfSquare()
+	{
+		int lastval = -1;
+		for(Row r : matrix)
+		{
+			if(lastval != -1 && r.values.size() != lastval)
+			{
+				return false;
+			}
+			lastval = r.values.size();
+		}
+		return matrix.size() == lastval;
+	}
+	
+	static Row getRowbyId(int i)
+	{
+		return matrix.get(i);
+	}
+	
+	static Row getRowbyName(String name)
+	{
+		for(Row r : matrix)
+		{
+			if(name.equals(r.rowName))
+			{
+				return r;
+			}
+		}
+		return null;
+	}
+	static int matrixSize()
+	{
+		return matrix.size();
+	}
+	
+	static void calcOutInMatrix()
+	{
+		for(Row r : matrix)
+		{
+			r.calcOutgoing();
+		}
+	}
+}
--- lab7/src/Row.java
+++ lab7/src/Row.java
@@ -1,17 +1,9 @@
 import java.util.ArrayList;
 
-/*
- * Call methods in this order:
- * initMatrix()
- * addRow() until matrix full
- * checkIfSquare()  after matrix is full
- * calcOutInMatrix() to get outgoing links
- * 
- * and THEN, you may perform  PageRank
- */
+
 public class Row
 {
-	private static ArrayList<Row> matrix;
+	
 	
 	String rowName;
 	ArrayList<Link> values;
@@ -24,12 +16,12 @@ public class Row
 
 	
 	
-	Row(String name)
+	Row(String name, int matrixSize)
 	{
 		rowName = name;
 		outgoing = 0;
 		incoming = 0;
-		values = new ArrayList<Link>();
+		values = new ArrayList<Link>(matrixSize);
 		id = -1;
 	}
 	
@@ -42,76 +34,16 @@ public class Row
 		id = -1;
 	}
 	
-	static void initMatrix()
-	{
-		matrix = new ArrayList<Row>();
-	}
 	
-	static void addRow(Row val)
-	{
-		if(val.id != -1 )
-		{
-			System.err.println("value already in matrix");
-			System.exit(-1);
-		}
-		matrix.add(val);
-		val.id = matrix.size();
-	}
-	
-	
-	
-	static boolean checkIfSquare()
-	{
-		int lastval = -1;
-		for(Row r : matrix)
-		{
-			if(lastval != -1 && r.values.size() != lastval)
-			{
-				return false;
-			}
-			lastval = r.values.size();
-		}
-		return matrix.size() == lastval;
-	}
-	
-	static Row getRowbyId(int i)
-	{
-		return matrix.get(i);
-	}
-	
-	static Row getRowbyName(String name)
-	{
-		for(Row r : matrix)
-		{
-			if(name.equals(r.rowName))
-			{
-				return r;
-			}
-		}
-		return null;
-	}
-	static int matrixSize()
-	{
-		return matrix.size();
-	}
-	
-	static void calcOutInMatrix()
-	{
-		for(Row r : matrix)
-		{
-			r.calcOutgoing();
-		}
-	}
-	
-	private void calcOutgoing()
+	public void calcOutgoing()
 	{
 		//for each value, check its brother column if it has the same number
 		for(int i = 0; i < values.size(); i++)
 		{
-			if(values.get(i).value != -1 && matrix.get(i).id != id)
+			if(values.get(i).value != -1 && Matrix.matrix.get(i).id != id)
 			{
 				int thisval = values.get(i).value;
-				int othval = matrix.get(i).values.get(id).value;
+				int othval = Matrix.matrix.get(i).values.get(id).value;
 				
 				//undirected graph
 				if(thisval == othval)
@@ -157,6 +89,7 @@ public class Row
 	{
 		this.values.add(index, newVal);
 	}
+
 	
 	public int getLinkValue(int valId)
 	{
@@ -166,6 +99,11 @@ public class Row
 	public int getLinkRank(int valId)
 	{
 		return values.get(valId).rank;
+	}
+	
+	public Link getLink(int valId)
+	{
+		return values.get(valId);
 	}
 	
 	
