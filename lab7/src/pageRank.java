04d30547a214d3268afcddc62857ba69fef53798
Ryuho Kudo <ryuhokudo@gmail.com>
2010-12-02 18:24:23 -0800

    simple matrix fill code for pageRank

--- lab7/src/pageRank.java
+++ lab7/src/pageRank.java
@@ -1,11 +1,33 @@
 
 
 public class pageRank {
-	// pageRank <fileName>
+    static final double epsilon = 0.001;
+    static final double d = 0.95;
+    // pageRank <fileName>
 	static CSV file;
 	public static void main(String args[]){
-		file = new CSV("data/NCAA_football.csv");
+		Matrix.initMatrix();
+	    
+	    file = new CSV("data/NCAA_football.csv");
 		
 		System.out.println(file.toString());
+		
+		System.out.println("size="+file.rows.size());
+		
+		int matrixSize = file.rows.size();
+		
+		for(int i = 0; i < file.rows.size(); i++){
+		    Row tempRow = new Row(file.rows.get(i),matrixSize);
+		    Matrix.addRow(tempRow);
+		}
+		
+		
+		for(int i = 0; i < file.data.size(); i++){
+		    Row tempRow = Matrix.getRowbyName(file.data.get(i).get(0));
+		    Link tempLink = new Link(Integer.parseInt(file.data.get(i).get(1)));
+		    tempRow.addValue(tempLink, Matrix.getRowbyName(file.data.get(i).get(2)).id);
+		}
+		
+		
 	}
 }
