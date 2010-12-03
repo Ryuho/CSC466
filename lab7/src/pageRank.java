bf02e6274870037939c06f8dad52c1bb058e4d74
Ryuho Kudo <ryuhokudo@gmail.com>
2010-12-02 18:58:07 -0800

    made it directed/undirected bug fix

--- lab7/src/pageRank.java
+++ lab7/src/pageRank.java
@@ -6,13 +6,23 @@ public class pageRank {
     // pageRank <fileName>
 	static CSV file;
 	public static void main(String args[]){
-		Matrix.initMatrix();
+	    boolean isDirected = false;
+	    
+	    if(args.length == 1){
+	        file = new CSV(args[0]);
+	    }
+	    else if(args.length == 2){
+	        file = new CSV(args[0]);
+	        isDirected = true;
+	    }
+	    
+		Matrix.initMatrix(isDirected);
 	    
 	    file = new CSV("data/NCAA_football.csv");
 		
-		System.out.println(file.toString());
+		//System.out.println(file.toString());
 		
-		System.out.println("size="+file.rows.size());
+		//System.out.println("size="+file.rows.size());
 		
 		int matrixSize = file.rows.size();
 		
@@ -24,9 +34,18 @@ public class pageRank {
 		
 		for(int i = 0; i < file.data.size(); i++){
 		    Row tempRow = Matrix.getRowbyName(file.data.get(i).get(0));
+		    Row tempRow2 = null;
 		    Link tempLink = new Link(Integer.parseInt(file.data.get(i).get(1)));
+		    Link tempLink2 = null;
 		    tempRow.addValue(tempLink, Matrix.getRowbyName(file.data.get(i).get(2)).id);
+		    if(isDirected){
+		        tempRow2 = Matrix.getRowbyName(file.data.get(i).get(2));
+		        tempLink2 = new Link(Integer.parseInt(file.data.get(i).get(3)));
+		        tempRow2.addValue(tempLink2, Matrix.getRowbyName(file.data.get(i).get(0)).id);
+		    }
+		    
 		}
+		
 		
 		
 	}
